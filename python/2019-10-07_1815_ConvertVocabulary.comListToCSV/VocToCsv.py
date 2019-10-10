07c1746e9cb8d609dfb502b898bbaafe2bf97fb3
marleeeeeey <marleeeeeey@gmail.com>
2019-10-10 03:14:42 +0300

    python anki parser: refactoring

--- python/2019-10-07_1815_ConvertVocabulary.comListToCSV/VocToCsv.py
+++ python/2019-10-07_1815_ConvertVocabulary.comListToCSV/VocToCsv.py
@@ -5,113 +5,143 @@ from google_speech import Speech
 import os
 import random
 
-# defines
 class Bucket:
     def __init__(self):
         self.word = ""
         self.word_translation = ""
+        self.path_to_sound = ""
         self.meaning = ""
+        self.meaning_translation = ""
         self.example = ""
         self.example_translation = ""
-        self.path_to_sound = ""
 
-def translate(source):
-    translator = Translator()
-    result = translator.translate(source , src='en', dest='ru')
-    return result.text
-    
-def print_bucket(b):
-    print(" word:", b.word, "\n", "meaning:", b.meaning, "\n",
-        "example:", b.example, "\n", "example_translation:", b.example_translation, "\n")
+    def print(self):
+        print(" word:", self.word, "\n",
+              "meaning:", self.meaning, "\n",
+              "example:", self.example, "\n",
+              "example_translation:", self.example_translation)
 
-def randomId(stringLength=10):
-    letters = "123456789"
-    result = ''.join(random.choice(letters) for i in range(stringLength))
-    return int(result)
-    
-# parse args
-importFileName = sys.argv[1]
-print("importFileName=", importFileName)
-exportFileName = importFileName.rstrip(".txt") + "_export.apkg"
-print("exportFileName=", exportFileName)
+    def process(self):
+        print("Processing word:", self.word)
+        self.generate_translations()
+        self.generate_speech()
 
-# read to list of lines
-importArray = []
-with open(importFileName, 'r') as reader:
-     for line in reader.readlines():        
-        importArray.append(line.rstrip())
+    def generate_translations(self):
+        self.example_translation = Helper.translate(self.example)
+        self.meaning_translation = Helper.translate(self.meaning)
+        self.word_translation = Helper.translate(self.word)
 
-# convert to list of Buckets
-buckets = []
-counter = 1
-bucket = Bucket()
-for line in importArray:
-    type = counter % 3
-    if type == 1:
-        bucket.word = line
-    elif type == 2:
-        bucket.meaning = line
-    elif type == 0:
-        bucket.example = line
-        buckets.append(bucket)
+    def generate_speech(self):
+        speech = Speech(self.word, "en");
+        self.path_to_sound = self.word + ".mp3"
+        speech.save(self.path_to_sound)
+
+    def generate_picture(self):
+        # todo
+        return
+
+
+class Helper:
+    @staticmethod
+    def translate(source, src='en', dest='ru'):
+        translator = Translator()
+        result = translator.translate(source, src, dest)
+        return result.text
+
+    @staticmethod
+    def get_random_id(stringLength=10):
+        letters = "123456789"
+        result = ''.join(random.choice(letters) for i in range(stringLength))
+        return int(result)
+
+    @staticmethod
+    def get_import_file_name():
+        return sys.argv[1]
+
+
+class Converter:
+    def start(self, import_file_name):
+        importArray = self.read_lines(import_file_name)
+        buckets = self.read_buckets(importArray)
+        deck_name = import_file_name.rstrip(".txt")
+        anki_manager = AnkiManager(deck_name)
+        export_file_name = deck_name + ".apkg"
+        anki_manager.save_anki_package(buckets, export_file_name)
+
+    def read_lines(self, import_file_name):
+        importArray = []
+        with open(import_file_name, 'r') as reader:
+            for line in reader.readlines():
+                importArray.append(line.rstrip())
+        return importArray
+
+    def read_buckets(self, importArray):
+        buckets = []
+        counter = 1
         bucket = Bucket()
-    counter += 1
+        for line in importArray:
+            type = counter % 3
+            if type == 1:
+                bucket.word = line
+            elif type == 2:
+                bucket.meaning = line
+            elif type == 0:
+                bucket.example = line
+                bucket.process()
+                buckets.append(bucket)
+                bucket = Bucket()
+            counter += 1
+        print("Processed", len(buckets), "words")
+        return buckets
 
 
-# updated list of audio files        
-# add translate and path to audio
-list_of_audio_files = []
-for bucket in buckets:
-    bucket.example_translation = translate(bucket.example)      
-    bucket.word_translation = translate(bucket.word)  
-    speech = Speech(bucket.word, "en");
-    sound_name = bucket.word+".mp3"
-    speech.save(sound_name)
-    bucket.path_to_sound = sound_name
-    list_of_audio_files.append(sound_name)
-    print_bucket(bucket)
+class AnkiManager:
+    def print(self):
+        print("model_id=", self.model_id)
+        print("deck_id =", self.deck_id)
+
+    def __init__(self, deck_name):
+        self.model_id = Helper.get_random_id()
+        self.deck_id = Helper.get_random_id()
+        self.model_name = "marleeeeeey@gmail.com export"
+        self.my_model = genanki.Model(
+            self.model_id,
+            self.model_name,
+            fields=[
+                {'name': 'Question'},
+                {'name': 'Answer'},
+                {'name': 'MyMedia'},
+            ],
+            templates=[
+                {
+                    'name': 'Card 1',
+                    'qfmt': '{{Question}}<br>{{MyMedia}}',
+                    'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+                },
+            ])
+        self.my_deck = genanki.Deck(self.deck_id, deck_name)
+
+    def save_anki_package(self, buckets, export_file_name):
+        list_of_audio_files = []
+        for bucket in buckets:
+            first_side = bucket.word + "<br><br>" + bucket.example
+            second_side = bucket.word_translation + "<br><br>" + \
+                          bucket.example_translation + "<br><br>" + \
+                          bucket.meaning
+            sound_string = "[sound:" + bucket.path_to_sound + "]"
+            list_of_audio_files.append(bucket.path_to_sound)
+            note = genanki.Note(model=self.my_model, fields=[first_side, second_side, sound_string])
+            self.my_deck.add_note(note)
+        pack = genanki.Package(self.my_deck)
+        pack.media_files = list_of_audio_files
+        pack.write_to_file(export_file_name)
+        print("ANKI deck saved to", export_file_name);
+        # remove temporary files
+        for media_file in list_of_audio_files:
+            os.remove(media_file)
 
 
-# anki defaults
-model_id = randomId()
-deck_id  = randomId()
-
-print("model_id=", model_id)
-print("deck_id =", deck_id)
-
-my_model = genanki.Model(
-  model_id,
-  'https://www.vocabulary.com export',
-  fields=[
-    {'name': 'Question'},
-    {'name': 'Answer'},
-    {'name': 'MyMedia'}, 
-  ],
-  templates=[
-    {
-      'name': 'Card 1',
-      'qfmt': '{{Question}}<br>{{MyMedia}}',   
-      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-    },
-  ])
-
-my_deck = genanki.Deck(
-    deck_id,
-    importFileName.rstrip(".txt")
-)
-
-#export to anki
-for bucket in buckets:
-    first_side = bucket.word + "<br><br>" + bucket.example
-    second_side = bucket.word_translation + "<br><br>" + bucket.example_translation + "<br><br>" + bucket.meaning;
-    sound_string = "[sound:" + bucket.path_to_sound + "]"
-    note = genanki.Note(model=my_model,fields=[first_side, second_side, sound_string])
-    my_deck.add_note(note)
-pack = genanki.Package(my_deck)
-pack.media_files = list_of_audio_files
-pack.write_to_file(exportFileName)
-
-# remove temporary files
-for media_file in list_of_audio_files:
-    os.remove(media_file)
-    
+converter = Converter()
+import_file_name = Helper.get_import_file_name()
+print("Import file name is", import_file_name)
+converter.start(import_file_name)
