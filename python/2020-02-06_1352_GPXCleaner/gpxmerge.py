6b246633752ee7aac517c82a1d9a15e8597cc96b
Sergey Tyulenev <marleeeeeey@gmail.com>
2020-02-07 14:23:26 +0300

    add gpx_merge

---
+++ python/2020-02-06_1352_GPXCleaner/gpxmerge.py
@@ -0,0 +1,30 @@
+import gpxlib
+import argparse
+import ntpath
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_glob_mask", type=str, default="./", help="Input glob mask")
+    parser.add_argument("-o", "--output_dir", type=str, default="./output/", help="Output directory")
+    args = parser.parse_args()
+
+    gpx_files = gpxlib.get_file_list(args.input_glob_mask)
+    print('found files:', gpx_files)
+    print('file count:', len(gpx_files))
+
+    if len(gpx_files) < 2:
+        return
+
+    output_dir = args.output_dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    base_name = ntpath.basename(gpx_files[0])
+    export_gpx = output_dir + base_name
+    export_tree = gpxlib.get_merge_gpx_tree(gpx_files)
+    gpxlib.write_gpxtree(export_tree, export_gpx)
+
+
+main()
